c775bd733145a0381df8a7c65a3f37cc0f940c90
Yaroslav <85008083+Rerowros@users.noreply.github.com>
2024-09-22 21:59:42 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,20 +1,6 @@
 @echo off
-echo "Workaem"
-echo.
-echo *********************************************^^^^^^^^^^^^^^^^^^
-echo *                                            *
-echo *  ��������!                                 *
-echo *                                            *
-echo *  �� ��� ��஫�, cookie � ��筠�          *
-echo *  ���ଠ�� �뫨 �ࠤ���.                 *
-echo *                                            *
-echo *  ��� ����⠭������� ����㯠 � ��訬        *
-echo *  �����, ��������, �令��� � ����      *
-echo *  �� ⥫�䮭�:                              *
-echo *  ^^^^ 8800-555-3535 ^^^^                   *
-echo *                                            *
-echo *********************************************^^^^^^^^^^^^^^^^^^
-echo.
+echo "Work"
+
 call venv\Scripts\activate
 python bench.py
 pause
