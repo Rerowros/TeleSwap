7664b6c7e75c4c84f5337a7e0d49040eb4f9f956
Yaroslav <yp32895@gmail.com>
2024-09-30 23:01:20 +0700

    Refactor logging and error handling in TeleSwap.py

--- TeleSwap.py
+++ TeleSwap.py
@@ -1,25 +1,45 @@
+import logging
 from telethon.sync import TelegramClient, events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.errors import PersistentTimestampOutdatedError
 import json
 
+class Logger:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler("TeleSwap.log", encoding='utf-8'),
+            logging.StreamHandler()
+        ]
+    )
+logger = logging.getLogger(__name__)
 
+# КОНФИГ
 try:
     with open('config.json', 'r', encoding='utf-8') as config_file:
         config = json.load(config_file)
-        print("Содержимое config.json:", config)  # Отладочный вывод
+        logger.info("Содержимое config.json: %s", config) 
 except FileNotFoundError:
-    print("Файл config.json не найден.")
+    logger.error("Файл config.json не найден.")
     exit(1)
 except json.JSONDecodeError as e:
-    print("Ошибка декодирования JSON в файле config.json: (Поменяйте кодировку на UTF-8)", e)
+    logger.error("Ошибка декодирования JSON в файле config.json: (Поменяйте кодировку на UTF-8) %s", e)
     exit(1)
 
-
 device_model, system_version, app_version, lang_code, system_lang_code, channel_id, message_map, api_id, api_hash = (
-    config[key] for key in ["device_model", "system_version", "app_version", "lang_code", "system_lang_code", "channel_id", "message_map", "api_id", "api_hash"]
+    config[key] for key in 
+    ["device_model", 
+    "system_version", 
+    "app_version", 
+    "lang_code", 
+    "system_lang_code", 
+    "channel_id", 
+    "message_map", 
+    "api_id", 
+    "api_hash"]
 )
-
+# Объявление клиента телеграма с параметрами из конфига
 client = TelegramClient(
     'session_name',
     api_id,
@@ -31,25 +51,29 @@ client = TelegramClient(
     system_lang_code=system_lang_code
 )
 
-@client.on(events.NewMessage(outgoing=True))
-async def handler(event):
-    try:
-        message_text = event.message.message
-        if message_text in message_map:
-            await client.delete_messages(event.chat_id, [event.id])
-            message_id = message_map[message_text]
-            message = await client.get_messages(channel_id, ids=message_id)
-            if message:
-                if message.text:
-                    await client.send_message(event.chat_id, message.text)
-                elif message.media:
-                    await client.send_file(event.chat_id, message.media)
-                    
-    except PersistentTimestampOutdatedError:
-        print("РЕСТАРТ. Сессия была остановлена")
-        client.disconnect()
-        client.start()
-        client.run_until_disconnected()
+class TeleSwap:
+    @client.on(events.NewMessage(outgoing=True))
+    async def handler(event):
+        try:
+            message_text = event.message.message
+            if message_text in message_map:
+                await client.delete_messages(event.chat_id, [event.id])
+                message_id = message_map[message_text]
+                message = await client.get_messages(channel_id, ids=message_id)
+                if message:
+                    if message.text:
+                        logger.info("Отправка сообщения: %s", message.text) 
+                        await client.send_message(event.chat_id, message.text)
+                    elif message.media:
+                        logger.info("Отправлено медиа: %s", message_map[message_text]) 
+                        await client.send_file(event.chat_id, message.media)
+                        
+        except PersistentTimestampOutdatedError:
+            logger.warning("РЕСТАРТ. Сессия была остановлена")
+            client.disconnect()
+            client.start()
+            client.run_until_disconnected()
 
-client.start()
-client.run_until_disconnected()
+class main:
+    client.start()
+    client.run_until_disconnected()
